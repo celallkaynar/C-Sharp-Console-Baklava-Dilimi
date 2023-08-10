aea274e826523dde838eb01c13aa456e8938fc78
celall.kaynar <celalksoftware@gmail.com>
2023-08-10 17:58:58 +0300

    Baklava dilimi C#
    C# diliyle konsol üzerinden baklava dilimi çizme

---
+++ baklavaDilimi/baklavaDilimi/Program.cs
@@ -0,0 +1,27 @@
+﻿int boyut, i, j;
+Console.Write("Boyut Giriniz: ");
+boyut=Convert.ToInt16(Console.ReadLine());
+for (i = 1; i <= boyut; i++)
+{
+    for(int k=boyut; k >=i; k--)
+    {
+        Console.Write(" ");
+    }
+    for (j = 1; j <= i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+for (i = boyut-1; i >= 1; i--)
+{
+    for (int k = boyut; k >= i; k--)
+    {
+        Console.Write(" ");
+    }
+    for (j = 1; j <= i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
